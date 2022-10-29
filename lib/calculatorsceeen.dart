b26671cefed07caae969ad98a0c4fbad2a37a4b2
Morné le Roux <mornelerx@gmail.com>
2022-10-29 15:52:44 +0200

    Added Template Y Slider

--- lib/calculatorsceeen.dart
+++ lib/calculatorsceeen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -10,6 +10,8 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  int templateSizeX = 0;
+  int templateSizeY = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +19,46 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       appBar: AppBar(
         backgroundColor: Colors.amber,
         title: Center(child: Text("PIC Price Calculator")),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              "Template Size",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SliderTheme(
+              data: SliderThemeData(
+                  activeTrackColor: Colors.white,
+                  thumbColor: Colors.white,
+                  inactiveTrackColor: Colors.amber[700],
+                  overlayColor: Colors.white60),
+              child: Slider(
+                value: templateSizeY.toDouble(),
+                min: 0.0,
+                max: 120.0,
+                divisions: 12,
+                onChanged: (double newValue) {
+                  setState(() {
+                    templateSizeY = newValue.toInt();
+                  });
+                },
+              ),
+            ),
+            Text(
+              "Y = ${templateSizeY.toString()}",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28),
+            )
+          ],
+        ),
       ),
     );
   }
